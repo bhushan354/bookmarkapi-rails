3f99a4ba54c8274004463c193f9a8570993e60d1
bhushan354 <deshmukhumesh354@gmail.com>
2024-01-16 12:01:38 +0530

    Write your first request spec - 2

--- spec/requests/health_request_spec.rb
+++ spec/requests/health_request_spec.rb
@@ -1,4 +1,14 @@
 require 'rails_helper'
+# You can replace ‘Healths’ with any string you want, it is for your own documentation purpose.
+
+# The "type: :request " will tell RSpec that this is a system test.
+
+# Sometimes when testing a deeply nested JSON string response, we can make it easier to read by parsing it to a Ruby hash using JSON.parse like this :
+# # by default, JSON has string key like {"status": "online"},
+# # we then convert the key to symbol, {status: 'online'} for better readability, 
+# # and type less two quotes character
+# json = JSON.parse(response.body).deep_symbolize_keys
+
 
 RSpec.describe "Healths", type: :request do
   describe "GET /index" do
